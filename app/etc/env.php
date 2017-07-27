ff405c150f1f7e54ae1074ed6537bbf2f5036329
root <Jacob Pierce>
2017-07-27 18:09:15 -0400

    initial commit

---
+++ app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_urocxp',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'b1fde8dbe6b6be6a06768f8f55f9aa18',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento',
+        'username' => 'magento',
+        'password' => 'XTy5SepfTJ',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 27 Jul 2017 22:01:35 +0000',
+  ),
+);
